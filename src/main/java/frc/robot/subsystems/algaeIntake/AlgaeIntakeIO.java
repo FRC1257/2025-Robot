dcc4ec3de71de268a376147e53cb35c1ed2879b2
raghavmani0129 <raghavrhino2829@icloud.com>
2025-01-25 11:06:02 -0500

    Finished algae intake for now (1/25)

---
+++ src/main/java/frc/robot/subsystems/algaeIntake/AlgaeIntakeIO.java
@@ -0,0 +1,45 @@
+package frc.robot.subsystems.algaeIntake;
+import org.littletonrobotics.junction.AutoLog;
+
+
+public interface AlgaeIntakeIO {
+    @AutoLog
+    public static class AlgaeIntakeIOInputs {
+        /** 
+         * Some of these may be unnecessary if no
+         * NEOs are used.
+         */
+        public double velocityRadsPerSec = 0.0;
+        public double appliedVoltage = 0.0;
+        public double speedSetpoint = 0.0;
+        public double[] currentAmps = new double[] {};
+        public double[] tempCelcius = new double[] {};
+       
+    }
+    public default void updateInputs(AlgaeIntakeIOInputs inputs) {}
+    /** sets voltage to run motor if necessary */
+    public default void setVoltage(double voltage) {}
+    /** sets velocity setpoint */
+    public default void setPIDConstants(double p, double i, double d) {}
+    
+    public default void stop() {
+        setVoltage(0.0);
+    }
+
+    /** sets brake mode */
+    public default void setBrake(boolean brake) {}
+
+    public default void setSpeed(double speed) {}
+
+    public default void setP(double p) {}
+    
+    public default void setI(double i) {}
+
+    public default void setD(double d) {}
+
+    public default double getP() { return 0.0; }
+
+    public default double getI() { return 0.0; }
+
+    public default double getD() { return 0.0; }
+}
